599847b0e15130d227b9dbf3acbef8b6ae11432c
Samuli <samulipeltomaa@hotmail.com>
2021-10-28 22:03:28 +0300

    format fixes

--- fir/filter.c
+++ fir/filter.c
@@ -5,56 +5,56 @@
 #define x 128
 
 void fir_filter() {
-    int d0 = 0;
-    int d1 = 0;
-    int d2 = 0;
-    int d3 = 0;
-    int input_sample;
-    int filtered_sample;
-    int status = 1;
+	int d0 = 0;
+	int d1 = 0;
+	int d2 = 0;
+	int d3 = 0;
+	int input_sample;
+	int filtered_sample;
+	int status = 1;
 
-    while (status > 0) {
-        _TCE_FIFO_U8_STREAM_IN(0, input_sample, status);
+	while (status > 0) {
+		_TCE_FIFO_U8_STREAM_IN(0, input_sample, status);
 
-        d0 = d1;
-        d1 = d2;
-        d2 = d3;
-        d3 = input_sample - x;
+		d0 = d1;
+		d1 = d2;
+		d2 = d3;
+		d3 = input_sample - x;
 
-        filtered_sample = d0 * k0 + d1 * k1 + d2 * k2 + d3 * k3;
-        _TCE_FIFO_U8_STREAM_OUT((filtered_sample >> 8) + x);
-        _TCE_FIFO_U8_STREAM_IN(0, input_sample, status);
+		filtered_sample = d0 * k0 + d1 * k1 + d2 * k2 + d3 * k3;
+		_TCE_FIFO_U8_STREAM_OUT((filtered_sample >> 8) + x);
+		_TCE_FIFO_U8_STREAM_IN(0, input_sample, status);
 
-        d0 = d1;
-        d1 = d2;
-        d2 = d3;
-        d3 = input_sample - x;
+		d0 = d1;
+		d1 = d2;
+		d2 = d3;
+		d3 = input_sample - x;
 
-        filtered_sample = d0 * k0 + d1 * k1 + d2 * k2 + d3 * k3;
-        _TCE_FIFO_U8_STREAM_OUT((filtered_sample >> 8) + x);
-        _TCE_FIFO_U8_STREAM_IN(0, input_sample, status);
+		filtered_sample = d0 * k0 + d1 * k1 + d2 * k2 + d3 * k3;
+		_TCE_FIFO_U8_STREAM_OUT((filtered_sample >> 8) + x);
+		_TCE_FIFO_U8_STREAM_IN(0, input_sample, status);
 
-        d0 = d1;
-        d1 = d2;
-        d2 = d3;
-        d3 = input_sample - x;
+		d0 = d1;
+		d1 = d2;
+		d2 = d3;
+		d3 = input_sample - x;
 
-        filtered_sample = d0 * k0 + d1 * k1 + d2 * k2 + d3 * k3;
-        _TCE_FIFO_U8_STREAM_OUT((filtered_sample >> 8) + x);
-        _TCE_FIFO_U8_STREAM_IN(0, input_sample, status);
+		filtered_sample = d0 * k0 + d1 * k1 + d2 * k2 + d3 * k3;
+		_TCE_FIFO_U8_STREAM_OUT((filtered_sample >> 8) + x);
+		_TCE_FIFO_U8_STREAM_IN(0, input_sample, status);
 
-        d0 = d1;
-        d1 = d2;
-        d2 = d3;
-        d3 = input_sample - x;
+		d0 = d1;
+		d1 = d2;
+		d2 = d3;
+		d3 = input_sample - x;
 
-        filtered_sample = d0 * k0 + d1 * k1 + d2 * k2 + d3 * k3;
-        _TCE_FIFO_U8_STREAM_OUT((filtered_sample >> 8) + x);
-    }
+		filtered_sample = d0 * k0 + d1 * k1 + d2 * k2 + d3 * k3;
+		_TCE_FIFO_U8_STREAM_OUT((filtered_sample >> 8) + x);
+	}
 }
 
 void copy_header(int size) {
-    int i;
+	int i;
 	int temp;
 	int status;
 	for (i = 0; i < size; i++) {
